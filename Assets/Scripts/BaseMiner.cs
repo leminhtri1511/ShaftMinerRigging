be6ab19f45bc1c644ea9e078b23068002ce36ebc
Le Tri <trile20021511@gmail.com>
2025-03-31 12:11:48 +0700

    feat: add DOTween package

--- Assets/Scripts/BaseMiner.cs
+++ Assets/Scripts/BaseMiner.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BaseMiner : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private float moveSpeed = 5f;
+    private int initialCollectCapacity = 200;
+    private float goldCollectPerSecond = 50f;
+
+    public void MoveMiner(Vector3 newPosition)
     {
-        
+        transform.DOMove(newPosition, duration: 10f / moveSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
