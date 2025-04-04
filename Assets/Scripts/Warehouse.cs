fad5704731465d3fee689c6c190297bb3946b88e
Le Tri <trile20021511@gmail.com>
2025-04-04 15:46:07 +0700

    feat: add WarehouseMiner, animation & Warehouse class

---
+++ Assets/Scripts/Warehouse.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warehouse : MonoBehaviour
+{
+    [SerializeField] private Deposit elevatorDeposit;
+    [SerializeField] private Transform elevatorLocation;
+    [SerializeField] private Transform warehouseDepositLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
